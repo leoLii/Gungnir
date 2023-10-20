660fba5cef99c888d46be2d8d0ab07f17404f482
leoLii <leolii0704@gmail.com>
2023-10-20 15:02:01 +0800

    [Driver] Complete computePipeline.cpp.

---
+++ driver/bgiBase/computePipeline.cpp
@@ -0,0 +1,58 @@
+#include "computePipeline.h"
+
+GUNGNIR_NAMESPACE_OPEN_SCOPE
+
+BgiComputeShaderConstantsDesc::BgiComputeShaderConstantsDesc()
+    : byteSize(0)
+{
+}
+
+bool operator==(
+    const BgiComputeShaderConstantsDesc& lhs,
+    const BgiComputeShaderConstantsDesc& rhs)
+{
+    return lhs.byteSize == rhs.byteSize;
+}
+
+bool operator!=(
+    const BgiComputeShaderConstantsDesc& lhs,
+    const BgiComputeShaderConstantsDesc& rhs)
+{
+    return !(lhs == rhs);
+}
+
+BgiComputePipelineDesc::BgiComputePipelineDesc()
+    : shaderProgram()
+{
+}
+
+bool operator==(
+    const BgiComputePipelineDesc& lhs,
+    const BgiComputePipelineDesc& rhs)
+{
+    return lhs.debugName == rhs.debugName &&
+           lhs.shaderProgram == rhs.shaderProgram &&
+           lhs.shaderConstantsDesc == rhs.shaderConstantsDesc;
+}
+
+bool operator!=(
+    const BgiComputePipelineDesc& lhs,
+    const BgiComputePipelineDesc& rhs)
+{
+    return !(lhs == rhs);
+}
+
+BgiComputePipeline::BgiComputePipeline(BgiComputePipelineDesc const& desc)
+    : _descriptor(desc)
+{
+}
+
+BgiComputePipeline::~BgiComputePipeline() = default;
+
+BgiComputePipelineDesc const&
+BgiComputePipeline::GetDescriptor() const
+{
+    return _descriptor;
+}
+
+GUNGNIR_NAMESPACE_CLOSE_SCOPE
