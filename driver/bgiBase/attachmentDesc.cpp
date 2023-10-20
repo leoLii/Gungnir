aee34bb06733116d88d05d74e27933a41d96ace6
leoLii <leolii0704@gmail.com>
2023-10-20 12:00:35 +0800

    [Driver] Add attachmentDesc files.

---
+++ driver/bgiBase/attachmentDesc.cpp
@@ -0,0 +1,57 @@
+#include "attachmentDesc.h"
+
+#include <ostream>
+
+GUNGNIR_NAMESPACE_OPEN_SCOPE
+
+bool operator==(
+    const BgiAttachmentDesc& lhs,
+    const BgiAttachmentDesc& rhs) 
+{
+    return  lhs.format == rhs.format &&
+            lhs.usage == rhs.usage &&
+            lhs.clearValue == rhs.clearValue &&
+            lhs.colorMask == rhs.colorMask &&
+            lhs.loadOp == rhs.loadOp &&
+            lhs.storeOp == rhs.storeOp &&
+            lhs.blendEnabled == rhs.blendEnabled &&
+            lhs.srcColorBlendFactor == rhs.srcColorBlendFactor &&
+            lhs.dstColorBlendFactor == rhs.dstColorBlendFactor &&
+            lhs.colorBlendOp == rhs.colorBlendOp &&
+            lhs.srcAlphaBlendFactor == rhs.srcAlphaBlendFactor &&
+            lhs.dstAlphaBlendFactor == rhs.dstAlphaBlendFactor &&
+            lhs.alphaBlendOp == rhs.alphaBlendOp &&
+            lhs.blendConstantColor == rhs.blendConstantColor;
+}
+
+bool operator!=(
+    const BgiAttachmentDesc& lhs,
+    const BgiAttachmentDesc& rhs)
+{
+    return !(lhs == rhs);
+}
+
+std::ostream& operator<<(
+    std::ostream& out,
+    const BgiAttachmentDesc& attachment)
+{
+    out << "BgiAttachmentDesc: {"
+        << "format: " << attachment.format << ", "
+        << "usage: " << attachment.usage << ", "
+        << "clearValue: " << attachment.clearValue << ", "
+        << "colorMask: " << attachment.colorMask << ", "
+        << "loadOp: " << attachment.loadOp << ", "
+        << "storeOp: " << attachment.storeOp << ", "
+        << "blendEnabled: " << attachment.blendEnabled << ", "
+        << "srcColorBlendFactor: " << attachment.srcColorBlendFactor << ", "
+        << "dstColorBlendFactor: " << attachment.dstColorBlendFactor << ", "
+        << "colorBlendOp: " << attachment.colorBlendOp << ", "
+        << "srcAlphaBlendFactor: " << attachment.srcAlphaBlendFactor << ", "
+        << "dstAlphaBlendFactor: " << attachment.dstAlphaBlendFactor << ", "
+        << "alphaBlendOp: " << attachment.alphaBlendOp << ", "
+        << "blendConstantColor: " << attachment.blendConstantColor <<
+    "}";
+    return out;
+}
+
+GUNGNIR_NAMESPACE_CLOSE_SCOPE
---
+++ driver/bgiBase/attachmentDesc.h
@@ -0,0 +1,93 @@
+#ifndef GUNGNIR_DRIVER_BASE_ATTACHMENT_DESC_H
+#define GUNGNIR_DRIVER_BASE_ATTACHMENT_DESC_H
+
+#include "base.h"
+#include "enums.h"
+#include "types.h"
+#include "pxr/base/gf/vec4f.h"
+
+#include <vector>
+
+GUNGNIR_NAMESPACE_OPEN_SCOPE
+
+/// \struct HgiAttachmentDesc
+///
+/// Describes the properties of a framebuffer attachment.
+///
+/// <ul>
+/// <li>format:
+///   The format of the attachment.
+///   Must match what is set in HgiTextureDesc.</li>
+/// <li>usage:
+///   Describes how the texture is intended to be used.
+///   Must match what is set in HgiTextureDesc.</li>
+/// <li>loadOp:
+///   The operation to perform on the attachment pixel data prior to rendering.</li>
+/// <li>storeOp:
+///   The operation to perform on the attachment pixel data after rendering.</li>
+/// <li>clearValue:
+///   The value to clear the attachment with (r,g,b,a) or (depth,stencil,x,x)</li>
+/// <li>colorMask:
+///   Whether to permit or restrict writing to component channels.</li>
+/// <li>blendEnabled:
+///   Determines if a blend operation should be applied to the attachment.</li>
+/// <li> ***BlendFactor:
+///   The blend factors for source and destination.</li>
+/// <li> ***BlendOp: 
+///   The blending operation.</li>
+/// <li> blendConstantColor:
+///   The constant color for blend operations.</li>
+///
+struct BgiAttachmentDesc
+{
+    BgiAttachmentDesc() 
+    : format(BgiFormatInvalid)
+    , usage(0)
+    , loadOp(BgiAttachmentLoadOpLoad)
+    , storeOp(BgiAttachmentStoreOpStore)
+    , clearValue(0)
+    , colorMask(BgiColorMaskRed | BgiColorMaskGreen |
+                BgiColorMaskBlue | BgiColorMaskAlpha)
+    , blendEnabled(false)
+    , srcColorBlendFactor(BgiBlendFactorZero)
+    , dstColorBlendFactor(BgiBlendFactorZero)
+    , colorBlendOp(BgiBlendOpAdd)
+    , srcAlphaBlendFactor(BgiBlendFactorZero)
+    , dstAlphaBlendFactor(BgiBlendFactorZero)
+    , alphaBlendOp(BgiBlendOpAdd)
+    , blendConstantColor(0.0f, 0.0f, 0.0f, 0.0f)
+    {}
+
+    BgiFormat format;
+    BgiTextureUsage usage;
+    BgiAttachmentLoadOp loadOp;
+    BgiAttachmentStoreOp storeOp;
+    GfVec4f clearValue;
+    BgiColorMask colorMask;
+    bool blendEnabled;
+    BgiBlendFactor srcColorBlendFactor;
+    BgiBlendFactor dstColorBlendFactor;
+    BgiBlendOp colorBlendOp;
+    BgiBlendFactor srcAlphaBlendFactor;
+    BgiBlendFactor dstAlphaBlendFactor;
+    BgiBlendOp alphaBlendOp;
+    GfVec4f blendConstantColor;
+};
+
+using BgiAttachmentDescVector = std::vector<BgiAttachmentDesc>;
+
+bool operator==(
+    const BgiAttachmentDesc& lhs,
+    const BgiAttachmentDesc& rhs);
+
+bool operator!=(
+    const BgiAttachmentDesc& lhs,
+    const BgiAttachmentDesc& rhs);
+
+std::ostream& operator<<(
+    std::ostream& out,
+    const BgiAttachmentDesc& attachment);
+
+GUNGNIR_NAMESPACE_CLOSE_SCOPE
+
+#endif
