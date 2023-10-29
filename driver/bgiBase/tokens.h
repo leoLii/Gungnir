1946300dcbf10c2c8d0bcb16b1264b92f856fe80
leoLii <leolii0704@gmail.com>
2023-10-29 17:36:18 +0800

    [Driver] Add token files.

---
+++ driver/bgiBase/tokens.h
@@ -0,0 +1,17 @@
+#ifndef GUNGNIR_DRIVER_BASE_TOKENS_H
+#define GUNGNIR_DRIVER_BASE_TOKENS_H
+
+#include "core/base.h"
+
+GUNGNIR_NAMESPACE_OPEN_SCOPE
+
+#define BGI_TOKENS    \
+    (taskDriver)      \
+    (renderDriver)    \
+    (OpenGL)          \
+    (Metal)           \
+    (Vulkan)
+
+GUNGNIR_NAMESPACE_CLOSE_SCOPE
+
+#endif
