9a6086b9dc51945fbe426349487686b594f6edd0
leoLii <leolii0704@gmail.com>
2023-11-03 15:59:25 +0800

    [Core] Add platform defines.

--- core/defines.h
+++ core/defines.h
@@ -4,6 +4,22 @@
 #define GUNGNIR_NAMESPACE_OPEN_SCOPE namespace gungnir {
 #define GUNGNIR_NAMESPACE_CLOSE_SCOPE }
 
+//
+// OS
+//
 
-
+#if defined(__linux__)
+#define ARCH_OS_LINUX
+#elif defined(__APPLE__)
+#include "TargetConditionals.h"
+#define ARCH_OS_DARWIN
+#if TARGET_OS_IPHONE
+#define ARCH_OS_IOS
+#else
+#define ARCH_OS_OSX
 #endif
+#elif defined(_WIN32) || defined(_WIN64)
+#define ARCH_OS_WINDOWS
+#endif
+
+#endif // GUNGNIR_CORE_DEFINES_H
---
+++ driver/bgiVulkan/vulkanBridge.h
@@ -0,0 +1,27 @@
+#ifndef GUNGNIR_DRIVER_VULKAN_VULKAN_BRIDGE_H
+#define GUNGNIR_DRIVER_VULKAN_VULKAN_BRIDGE_H
+
+#include "core/base.h"
+
+// Define the platform for Vulkan so vulkan.h below picks the correct includes.
+#if defined(ARCH_OS_WINDOWS)
+#define VK_USE_PLATFORM_WIN32_KHR
+#elif defined(ARCH_OS_LINUX)
+#define VK_USE_PLATFORM_XLIB_KHR
+#elif defined(ARCH_OS_OSX)
+#define VK_USE_PLATFORM_MACOS_MVK
+#else
+#error Unsupported Platform
+#endif
+
+#include <vulkan/vulkan.h>
+
+#include <vk_mem_alloc.h>
+
+// Use the default allocator (nullptr)
+inline VkAllocationCallbacks*
+BgiVulkanAllocator() {
+    return nullptr;
+}
+
+#endif // GUNGNIR_DRIVER_VULKAN_VULKAN_BRIDGE_H
