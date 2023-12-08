e1536c5da61c7f07fb3a81d1546ee5a877293192
leoLii <leolii0704@gmail.com>
2023-12-08 18:04:06 +0800

    [Base] New driver based on falcor.

---
+++ common/Enum.h
@@ -0,0 +1,161 @@
+#pragma once
+
+#include "Error.h"
+#include "Utils/Logger.h"
+#include <span>
+#include <string>
+#include <string_view>
+#include <type_traits>
+#include <algorithm>
+#include <vector>
+#include <fmt/core.h>
+
+namespace Falcor
+{
+// Helper using ADL to find EnumInfo in other namespaces.
+template<typename T>
+using EnumInfo = decltype(falcorFindEnumInfoADL(std::declval<T>()));
+
+template<typename, typename = void>
+struct has_enum_info : std::false_type
+{};
+
+template<typename T>
+struct has_enum_info<T, std::void_t<decltype(EnumInfo<T>::items)>> : std::true_type
+{};
+
+template<typename T>
+inline constexpr bool has_enum_info_v = has_enum_info<T>::value;
+
+/**
+ * Convert an enum value to a string.
+ * Throws if the enum value is not found in the registered enum information.
+ */
+template<typename T, std::enable_if_t<has_enum_info_v<T>, bool> = true>
+inline const std::string& enumToString(T value)
+{
+    const auto& items = EnumInfo<T>::items();
+    auto it = std::find_if(items.begin(), items.end(), [value](const auto& item) { return item.first == value; });
+    if (it == items.end())
+        FALCOR_THROW("Invalid enum value {}", int(value));
+    return it->second;
+}
+
+/**
+ * Convert a string to an enum value.
+ * Throws if the string is not found in the registered enum information.
+ */
+template<typename T, std::enable_if_t<has_enum_info_v<T>, bool> = true>
+inline T stringToEnum(std::string_view name)
+{
+    const auto& items = EnumInfo<T>::items();
+    auto it = std::find_if(items.begin(), items.end(), [name](const auto& item) { return item.second == name; });
+    if (it == items.end())
+        FALCOR_THROW("Invalid enum name '{}'", name);
+    return it->first;
+}
+
+/**
+ * Check if an enum has a value with the given name.
+ */
+template<typename T, std::enable_if_t<has_enum_info_v<T>, bool> = true>
+inline bool enumHasValue(std::string_view name)
+{
+    const auto& items = EnumInfo<T>::items();
+    auto it = std::find_if(items.begin(), items.end(), [name](const auto& item) { return item.second == name; });
+    return it != items.end();
+}
+
+/**
+ * Convert an flags enum value to a list of strings.
+ * Throws if any of the flags are not found in the registered enum information.
+ */
+template<typename T, std::enable_if_t<has_enum_info_v<T>, bool> = true>
+inline std::vector<std::string> flagsToStringList(T flags)
+{
+    std::vector<std::string> list;
+    const auto& items = EnumInfo<T>::items();
+    for (const auto& item : items)
+    {
+        if (is_set(flags, item.first))
+        {
+            list.push_back(item.second);
+            flip_bit(flags, item.first);
+        }
+    }
+    if (flags != T(0))
+        FALCOR_THROW("Invalid enum flags value {}", int(flags));
+    return list;
+}
+
+/**
+ * Convert a list of strings to a flags enum value.
+ * Throws if any of the strings are not found in the registered enum information.
+ */
+template<typename T, std::enable_if_t<has_enum_info_v<T>, bool> = true>
+inline T stringListToFlags(const std::vector<std::string>& list)
+{
+    T flags = T(0);
+    for (const auto& name : list)
+        flags |= stringToEnum<T>(name);
+    return flags;
+}
+
+} // namespace Falcor
+
+/**
+ * Define enum information. This is expected to be used as follows:
+ *
+ * enum class Foo { A, B, C };
+ * FALCOR_ENUM_INFO(Foo, {
+ *     { Foo::A, "A" },
+ *     { Foo::B, "B" },
+ *     { Foo::C, "C" },
+ * })
+ */
+#define FALCOR_ENUM_INFO(T, ...)                                    \
+    struct T##_info                                                 \
+    {                                                               \
+        static fstd::span<std::pair<T, std::string>> items()        \
+        {                                                           \
+            static std::pair<T, std::string> items[] = __VA_ARGS__; \
+            return {std::begin(items), std::end(items)};            \
+        }                                                           \
+    };
+
+/**
+ * Register enum information to be used with helper functions.
+ * This needs to be placed outside of any structs but within the
+ * namespace of the enum:
+ *
+ * namespace ns
+ * {
+ * struct Bar
+ * {
+ *     enum class Foo { A, B, C };
+ *     FALCOR_ENUM_INFO(Foo, ...)
+ * };
+ *
+ * FALCOR_ENUM_REGISTER(Bar::Foo)
+ * } // namespace ns
+ *
+ * Registered enums can be converted to/from strings using:
+ * - enumToString<Enum>(Enum value)
+ * - stringToEnum<Enum>(std::string_view name)
+ */
+#define FALCOR_ENUM_REGISTER(T)                                            \
+    constexpr T##_info falcorFindEnumInfoADL [[maybe_unused]] (T) noexcept \
+    {                                                                      \
+        return T##_info{};                                                 \
+    }
+
+/// Enum formatter.
+template<typename T>
+struct fmt::formatter<T, std::enable_if_t<Falcor::has_enum_info_v<T>, char>> : formatter<std::string>
+{
+    template<typename FormatContext>
+    auto format(const T& e, FormatContext& ctx)
+    {
+        return formatter<std::string>::format(Falcor::enumToString(e), ctx);
+    }
+};
---
+++ driver/slangDriver/Types.h
@@ -0,0 +1,144 @@
+#pragma once
+
+#include "common/base.h"
+#include "Core/Enum.h"
+
+GUNGNIR_NAMESPACE_OPEN_SCOPE
+
+enum class ShaderModel : uint32_t
+{
+    Unknown = 0,
+    SM6_0 = 60,
+    SM6_1 = 61,
+    SM6_2 = 62,
+    SM6_3 = 63,
+    SM6_4 = 64,
+    SM6_5 = 65,
+    SM6_6 = 66,
+    SM6_7 = 67,
+};
+FALCOR_ENUM_INFO(
+    ShaderModel,
+    {
+        {ShaderModel::Unknown, "Unknown"},
+        {ShaderModel::SM6_0, "SM6_0"},
+        {ShaderModel::SM6_1, "SM6_1"},
+        {ShaderModel::SM6_2, "SM6_2"},
+        {ShaderModel::SM6_3, "SM6_3"},
+        {ShaderModel::SM6_4, "SM6_4"},
+        {ShaderModel::SM6_5, "SM6_5"},
+        {ShaderModel::SM6_6, "SM6_6"},
+        {ShaderModel::SM6_7, "SM6_7"},
+    }
+);
+FALCOR_ENUM_REGISTER(ShaderModel);
+
+inline uint32_t getShaderModelMajorVersion(ShaderModel sm)
+{
+    return uint32_t(sm) / 10;
+}
+inline uint32_t getShaderModelMinorVersion(ShaderModel sm)
+{
+    return uint32_t(sm) % 10;
+}
+
+/**
+ * Falcor shader types
+ */
+enum class ShaderType
+{
+    Vertex,        ///< Vertex shader
+    Pixel,         ///< Pixel shader
+    Geometry,      ///< Geometry shader
+    Hull,          ///< Hull shader (AKA Tessellation control shader)
+    Domain,        ///< Domain shader (AKA Tessellation evaluation shader)
+    Compute,       ///< Compute shader
+    RayGeneration, ///< Ray generation shader
+    Intersection,  ///< Intersection shader
+    AnyHit,        ///< Any hit shader
+    ClosestHit,    ///< Closest hit shader
+    Miss,          ///< Miss shader
+    Callable,      ///< Callable shader
+    Count          ///< Shader Type count
+};
+FALCOR_ENUM_INFO(
+    ShaderType,
+    {
+        {ShaderType::Vertex, "Vertex"},
+        {ShaderType::Pixel, "Pixel"},
+        {ShaderType::Geometry, "Geometry"},
+        {ShaderType::Hull, "Hull"},
+        {ShaderType::Domain, "Domain"},
+        {ShaderType::Compute, "Compute"},
+        {ShaderType::RayGeneration, "RayGeneration"},
+        {ShaderType::Intersection, "Intersection"},
+        {ShaderType::AnyHit, "AnyHit"},
+        {ShaderType::ClosestHit, "ClosestHit"},
+        {ShaderType::Miss, "Miss"},
+        {ShaderType::Callable, "Callable"},
+    }
+);
+FALCOR_ENUM_REGISTER(ShaderType);
+
+enum class DataType
+{
+    int8,
+    int16,
+    int32,
+    int64,
+    uint8,
+    uint16,
+    uint32,
+    uint64,
+    float16,
+    float32,
+    float64,
+};
+FALCOR_ENUM_INFO(
+    DataType,
+    {
+        {DataType::int8, "int8"},
+        {DataType::int16, "int16"},
+        {DataType::int32, "int32"},
+        {DataType::int64, "int64"},
+        {DataType::uint8, "uint8"},
+        {DataType::uint16, "uint16"},
+        {DataType::uint32, "uint32"},
+        {DataType::uint64, "uint64"},
+        {DataType::float16, "float16"},
+        {DataType::float32, "float32"},
+        {DataType::float64, "float64"},
+    }
+);
+FALCOR_ENUM_REGISTER(DataType);
+
+enum class ComparisonFunc
+{
+    Disabled,     ///< Comparison is disabled
+    Never,        ///< Comparison always fails
+    Always,       ///< Comparison always succeeds
+    Less,         ///< Passes if source is less than the destination
+    Equal,        ///< Passes if source is equal to the destination
+    NotEqual,     ///< Passes if source is not equal to the destination
+    LessEqual,    ///< Passes if source is less than or equal to the destination
+    Greater,      ///< Passes if source is greater than to the destination
+    GreaterEqual, ///< Passes if source is greater than or equal to the destination
+};
+
+FALCOR_ENUM_INFO(
+    ComparisonFunc,
+    {
+        {ComparisonFunc::Disabled, "Disabled"},
+        {ComparisonFunc::Never, "Never"},
+        {ComparisonFunc::Always, "Always"},
+        {ComparisonFunc::Less, "Less"},
+        {ComparisonFunc::Equal, "Equal"},
+        {ComparisonFunc::NotEqual, "NotEqual"},
+        {ComparisonFunc::LessEqual, "LessEqual"},
+        {ComparisonFunc::Greater, "Greater"},
+        {ComparisonFunc::GreaterEqual, "GreaterEqual"},
+    }
+);
+FALCOR_ENUM_REGISTER(ComparisonFunc);
+
+GUNGNIR_NAMESPACE_CLOSE_SCOPE // namespace Falcor
